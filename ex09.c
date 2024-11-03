48d4cb18005876d2f97710eace44c5da8ede91ea
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-11-03 20:15:24 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    
+    float sum=0.0;
+    float average=0.0;
+    int number;
+    int counter=0;
+    
+    while(1)
+    {   
+        printf("Please enter a number(enter -1 for exit ) = ");
+        scanf("%d",&number);        
+        if(number==-1)
+        {
+            break;
+        }
+        sum=sum+number;
+        counter++;
+    }
+    
+    average= sum/ counter;
+    printf("Sum = %.2f\n ",sum);
+    printf("Average = %.2f ",average);
+    return 0;
+}
