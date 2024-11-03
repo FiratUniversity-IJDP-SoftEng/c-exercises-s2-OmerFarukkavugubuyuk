c6e4559908b56b2c21ea136a4c76ccf94fffaf3c
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-11-03 19:08:34 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+   int array[17];
+   int counter=0;
+   
+   for(int i=100;i>=50;i--)
+   {
+       if(i%3==0)
+       {
+           array[counter]=i;
+           counter++;
+           
+       }
+   }
+   
+   for(int j=0;j<17;j++)
+   {
+       printf("%d ",array[j]);
+   }
+
+    return 0;
+}
