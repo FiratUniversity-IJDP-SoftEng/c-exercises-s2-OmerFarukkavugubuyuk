fb25546acb8b6bd20152f3fe1e314f12bcd28b5d
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-11-03 21:39:46 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+     int number;
+     long long factorial=1;
+     
+     scanf("%d",&number);
+     
+     if(number<50)
+     {
+        for(int i=1;i<=number;i++)
+        {
+         factorial=factorial*i;
+        }
+        printf("Factorial = %lld ",factorial);
+     }else
+     {
+         printf("Please enter a number less than 50 ");
+     }
+     
+    return 0;
+}
