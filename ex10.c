ea11e62c84e821d25bba8e3124a6b4fdafbebca8
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-11-03 20:25:13 +0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+   int number=1;
+   for(number;number<=10;number++)
+   {
+        for(int i=1; i<=10;i++)
+            {    
+            printf("%3d ",number*i);
+            }
+        printf("\n");
+   }
+    return 0;
+}
