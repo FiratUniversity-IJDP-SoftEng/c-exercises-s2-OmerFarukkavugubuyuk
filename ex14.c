74be2817feb81c2baf7fea9ac9ba43f0c2bb1495
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-11-03 22:00:59 +0300

    Update ex14.c

--- ex14.c
+++ ex14.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+void displayFibonacci(int n) {
+    
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", fibonacci(i)); 
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    
+    printf("Enter a number = ");
+    scanf("%d", &n);
+
+
+    if (n < 0) {
+        printf("Please enter a positive number \n");
+        return 1;
+    }
+
+
+    int nthFibonacci = fibonacci(n);
+    printf("%d. Fibonacci number: %d\n", n, nthFibonacci);
+
+   
+    displayFibonacci(n);
+
+    return 0;
+}
 
