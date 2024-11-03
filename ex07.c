6b33fcff0da2e631221fc1d113eefd9b3ec231dc
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-11-03 19:20:37 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int column1(int i)
+   {
+       i=i*5;
+       return i;
+   }
+int column2(int i)
+   {
+      i= 100-((i-1)*10+1);
+       return i;
+   }
+int main() {
+    for(int j=1;j<=10;j++)
+    {
+        printf("%d,%d,%d\n",column1(j),column2(j),j);
+    }
+    return 0;
+}
