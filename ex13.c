1236ec81e9687df764fefd1ed8c1c2cb4ddf464b
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2024-11-03 21:46:20 +0300

    Update ex13.c

--- ex13.c
+++ ex13.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+long long factorial_recursive(int n) {
+    
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    
+    return n * factorial_recursive(n - 1);
+}
+
+int main() {
+    int number;
+    printf("Enter a number (less than 50): ");
+    scanf("%d", &number);
+
+    if (number < 50 && number >= 0) {
+        long long result = factorial_recursive(number);
+        printf("Factorial of  = %lld\n", result);
+    } else {
+        printf("Please enter a number less than 50.\n");
+    }
+
+    return 0;
+}
